cc191a8ba7d4c9057878e666190dd9545e400727
RZ2J7W <dale.sinnott@gm.com>
2019-01-29 20:48:48 +0000

    Added component, modified something else i think

--- ProjectSkyfire/src/scenes/GameScene.cpp
+++ ProjectSkyfire/src/scenes/GameScene.cpp
@@ -7,7 +7,17 @@ void GameScene::update(sf::Event* e)
 {
 	switch (gameState)
 	{
+	case GameScene::IN_DEALERSHIP:
 
+		break;
+	case GameScene::IN_DATE:
+
+		break;
+	case GameScene::END_SCREEN:
+
+		break;
+	default:
+		break;
 	}
 }
 void GameScene::render(std::shared_ptr<sf::RenderWindow> &window)
